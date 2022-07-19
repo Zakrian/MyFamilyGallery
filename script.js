9a49dccc71af28d78c812019f7b569cc8583c400
Zakrian <82257242+Zakrian@users.noreply.github.com>
2022-07-19 19:45:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var index = 0
+
+showImage(index)
+
+function showImage (i) {
+  index += 1
+  var images = document.querySelectorAll('.image')
+  var dots = document.querySelectorAll('.dot')
+
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = 'none'
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace('active', '')
+  }
+
+  if (index > images.length - 1) {
+    index = 0
+  }
+  if (index < 0) {
+    index = images.length - 1
+  }
+  images[index].style.display = 'block'
+  dots[index].className += ' active'
+}
